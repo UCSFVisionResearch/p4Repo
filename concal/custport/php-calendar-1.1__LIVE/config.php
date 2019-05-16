c32d46c0370b0455f452c1d93c50b5907b514273
jphamUCSF <jimmy.pham.ucsf@gmail.com>
2019-05-16 11:00:14 -0700

    pushing

---
+++ concal/custport/php-calendar-1.1__LIVE/config.php
@@ -0,0 +1,8 @@
+<?php
+define('SQL_HOST',     'localhost');
+define('SQL_USER',     'hui');
+define('SQL_PASSWD',   'calcal');
+define('SQL_DATABASE', 'calendar2');
+define('SQL_PREFIX',   'phpc2_');
+define('SQL_TYPE',     'mysql');
+?>
